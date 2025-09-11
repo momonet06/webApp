aebb920b9fedc876fec8e5e54ce94222552a1ee7
Jhinaoui Mohamed <momonet06@gmail.com>
2025-09-11 11:01:33 +0100

    next.conf modified

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import withRspack from "next-rspack";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   output: "standalone",
   typedRoutes: true,
   transpilePackages: ["lucide-react"],
@@ -29,20 +28,6 @@ const nextConfig: NextConfig = {
       },
     ],
     qualities: [25, 50, 75, 80, 90, 100],
-  },
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/api/:path*",
-          destination: process.env.NEXT_PUBLIC_BASE_URL + "/api/:path*",
-        },
-        {
-          source: "/uploads/:path*",
-          destination: process.env.NEXT_PUBLIC_BASE_URL + "/uploads/:path*",
-        },
-      ],
-    };
   },
 };
 
