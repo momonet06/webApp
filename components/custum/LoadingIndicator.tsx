7a3f34fa262bbe31397a934db746e17e998d2851
Jhinaoui Mohamed <momonet06@gmail.com>
2025-09-11 11:12:19 +0100

    usenextlink

--- components/custum/LoadingIndicator.tsx
+++ components/custum/LoadingIndicator.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { useLinkStatus } from "next/link";
-
-//import { useLinkStatus } from "next/link";
+import { useLinkStatus } from "next/dist/client/link";
 
 export default function LoadingIndicator() {
   const { pending } = useLinkStatus();
   return pending ? (
-    <div role="status" aria-label="Loading" aria-busy="true" className="spinner" />
+    <div
+      role="status"
+      aria-label="Loading"
+      aria-busy="true"
+      className="spinner"
+    />
   ) : null;
 }
